f4965e8da31937f5d9070b63f8c58c864a4d736e
Maurvick <82706197+Maurvick@users.noreply.github.com>
2022-08-22 00:02:59 +0300

    Complete easy task

---
+++ MoreOrLess/MoreOrLess/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace MoreOrLess
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int a, b;
+
+            Console.Write("Enter a value: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter b value: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("{0} > {1}", a, b);
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("{0} < {1}", a, b);
+            }
+            else
+            {
+                Console.WriteLine("{0} = {1}", a, b);
+            }
+
+            // Keep console open
+            Console.Read();
+        }
+    }
+}
